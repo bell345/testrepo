d5692c1820a7fea0b3fbcac7cf8703c2e5da33f8
Thomas Bell <tom.aus@outlook.com>
2014-12-18 17:22:22 +0800

    New repetitive Hello World function
    Hooray for examples!

--- program.py
+++ program.py
@@ -4,3 +4,9 @@ def somefunc(yay):
     return yay + " another part"
 
 somefunc("this and")
+def sayhi(num):
+    result = ""
+    for (i in range(num)):
+        result += "["+i+"] Hello world!\r\n"
+    return result
+sayhi(3)
